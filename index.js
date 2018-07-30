f49f5065360895dd273d220e7ceabb34cb30f50e
RyosukeCla <ryosukeclarinet@gmail.com>
2018-07-31 02:38:14 +0900

    add help command

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const chokidar = require('chokidar')
 const config = require('./lib/config')
 const Mockingcat = require('./lib/index')
 const chalk = require('chalk')
+const minimist = require('minimist')
+const argv = minimist(process.argv.splice(2))
+
+if (argv.help || argv.h) {
+  console.log(`
+Mockingcat
+  --help     (-h)
+  --port     (-p) : 8090
+  --srcdir   (-s) : ./mock
+  --baseurl  (-b) : /mock
+  --verbose  (-v) : true
+  `)
+  process.exit(0)
+}
 
 const watcher = chokidar.watch(config.default.srcDir, {
   ignored: /(^|[\/\\])\../,
