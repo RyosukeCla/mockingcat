cea1c1a9a7091aa5c0ad379bc51837add68987c5
Ryosuke Suzuki <ryosukeclarinet@gmail.com>
2018-08-02 23:13:57 +0900

    update lib

--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,8 +1,9 @@
-export declare const splitFilename: (filename: string) => {
+export declare const splitFilename: (filepath: string) => {
     name: string;
     extension: string;
 };
-export declare const processFilename: (filename: string) => string;
+export declare const processFilename: (filepath: string, srcDir: string, baseUrl: string) => string;
+export declare const requireWithoutCache: (filepath: string) => any;
 export declare const leftPad: (str: string, padding: number) => string;
 export declare const logError: (e: Error) => void;
 export declare const logStart: () => void;
